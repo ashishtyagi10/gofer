8a4e0284f6e403474223212e45d24bdbbe34604f
Ashish Tyagi <ashishtyagi10@gmail.com>
2023-01-05 19:56:32 -0500

    second commit with correct server

--- client/script.js
+++ client/script.js
@@ -69,7 +69,7 @@ const handleSubmit = async(e) =>{
     loader(messageDiv);
 
     // fetch the data from server -> bot response
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://gofer-8nbn.onrender.com/", {
         method: 'POST',
         headers:{
           'Content-Type':'application/json',  
